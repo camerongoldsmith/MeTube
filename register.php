7da0aefffe404f81c1a2dc6d70aa97c886d71d1b
ATWalters <austintwalters1998@gmail.com>
2021-04-05 19:40:52 -0400

    Added register.php and got started on user registration

---
+++ register.php
@@ -0,0 +1,32 @@
+<div id='bodyContent' class='body-content' style='text-align: center'>
+    <h3 class='registration-title'>Register for MeTube:</h3>
+    <form id="registrationForm" method="post">
+    <table class='registration-form' width='50%'>
+        <tr>
+            <td width="20%" class="registration-label">Username:</td>
+            <td width="80%"><input type="text" id="username" maxlength="30" name="username" class="form-control"><br></td>
+            <td width="20%" id="usernameValidation" class="registration-validation"></td>
+        </tr>
+        <tr>
+            <td width="20%" class="registration-label">Email:</td>
+            <td width="80%"><input type="text" id="email" maxlength="255" name="email" class="form-control"><br></td>
+            <td width="20%" id="emailValidation" class="registration-validation"></td>
+        </tr>
+        <tr>
+            <td width="20%" class="registration-label">Create Password:</td>
+            <td width="80%"><input type="password" id="password1" name="password1" class="form-control"><br></td>
+            <td width="20%" id="password1Validation" class="registration-validation"></td>
+        </tr>
+        <tr>
+            <td width="20%" class="registration-label">Repeat Password:</td>
+            <td width="80%"><input type="password" id="password2" name="password2" class="form-control"><br></td>
+            <td width="20%" id="password2Validation" class="registration-validation"></td>
+        </tr>
+        <tr>
+            <td width="20%"></td>
+            <td width="80%"><input name="submit" type="submit" value="Submit" class="btn btn-primary"></td>
+        </tr>
+    </table>
+    </form>
+</div>
+
